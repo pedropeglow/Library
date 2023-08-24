6b014ba18b33393ab69734e834f2a3b8940ac227
pedropeglow <pedropeglowm@gmail.com>
2023-08-24 11:57:00 -0300

    :sparkles: Exibindo livros de forma dinâmica.

--- app/main.js
+++ app/main.js
@@ -1,10 +1,28 @@
 let livros = []
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+const elementoParaInserirLivros = document.getElementById('livros')
 
 async function getBuscarLivrosAPI(){
     const res = await fetch(endpointAPI)
     livros = await res.json()
     console.table(livros)
+    exibirLivros(livros)
+}
+
+function exibirLivros(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">${livro.titulo}</h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco}</p>
+        <div class="tags">
+            <span class="tag">${livro.categoria}</span>
+        </div>
+        </div>
+        `
+    });
 }
 
 getBuscarLivrosAPI()
