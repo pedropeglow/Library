10a0c547ee64dcc31bd738875376d57eb29473e3
pedropeglow <pedropeglowm@gmail.com>
2023-08-28 17:22:43 -0300

    :sparkles: Adicionando função para filtrar livros.

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,7 @@
 const elementoParaInserirLivros = document.getElementById('livros')
 
 function exibirLivros(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
