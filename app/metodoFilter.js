f1dd63e6c13d91343463fe3e3d1accf7ef235170
pedropeglow <pedropeglowm@gmail.com>
2023-09-05 13:38:24 -0300

    :sparkles: Adicionando regra de valor total.

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,9 +1,33 @@
-const botoes = document.querySelectorAll('.btn')
-botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+const botoes = document.querySelectorAll(".btn");
+botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
 
 function filtrarLivros() {
-    const elementoBtn = document.getElementById(this.id)
-    const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
-    exibirLivros(livrosFiltrados);
+  const elementoBtn = document.getElementById(this.id);
+  const categoria = elementoBtn.value;
+  let livrosFiltrados =
+    categoria == "disponivel"
+      ? filtrarPorDisponibilidade()
+      : filtrarPorCategoria(categoria);
+  exibirLivros(livrosFiltrados);
+  if (categoria == "disponivel") {
+    exibirValorTotalDosLivrosDisponiveisNaTela();
+  }
+}
+
+function filtrarPorCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela() {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <section id="valor_total_livros_disponiveis">
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>
+  </section>
+    `;
 }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,12 +1,19 @@
-const elementoParaInserirLivros = document.getElementById('livros')
+const elementoParaInserirLivros = document.getElementById("livros");
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById(
+  "valor_total_livros_disponiveis"
+);
 
 function exibirLivros(listaDeLivros) {
-    elementoParaInserirLivros.innerHTML = ''
-    listaDeLivros.forEach(livro => {
-        let disponibilidade = livro.quantidade > 0 ? 'livro_imagem' : 'livro_imagem indisponivel'
-        elementoParaInserirLivros.innerHTML += `
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = "";
+  elementoParaInserirLivros.innerHTML = "";
+  listaDeLivros.forEach((livro) => {
+    let disponibilidade =
+      livro.quantidade > 0 ? "livro_imagem" : "livro_imagem indisponivel";
+    elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${
+      livro.alt
+    }" />
         <h2 class="livro__titulo">${livro.titulo}</h2>
         <p class="livro__descricao">${livro.autor}</p>
         <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
@@ -14,7 +21,6 @@ function exibirLivros(listaDeLivros) {
             <span class="tag">${livro.categoria}</span>
         </div>
         </div>
-        `
-    });
+        `;
+  });
 }
-
